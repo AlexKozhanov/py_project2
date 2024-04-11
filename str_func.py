933fd8823388a03f5a989d2611972f70c163a535
AlexKozhanov <47736169+AlexKozhanov@users.noreply.github.com>
2024-04-11 21:51:16 +0300

    add docstring

--- str_func.py
+++ str_func.py
@@ -1,9 +1,24 @@
 def upper_str(user_str):
+    """
+    Возвращает строку сменив все элементы на заглавные
+     
+    Parameters
+    ----------
+    user_str : str
+        строка
+ 
+    Returns
+    -------
+    str
+        строка с заглавными символами
+    """
+    
     user_str = user_str.upper()
     return user_str
 
 
 def main():
+# Ход основной программы
     print("Введите строку")
     user_input = input()
     user_input = upper_str(user_input)
