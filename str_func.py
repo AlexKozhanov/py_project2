973f4f5cb72f9bc7ab1e7703f32a732f1b697153
Admin <geniusalex59@gmail.com>
2024-04-11 22:59:56 +0300

    conflict correction

--- str_func.py
+++ str_func.py
@@ -1,24 +1,15 @@
 def upper_str(user_str):
     """
     Возвращает строку сменив все элементы на заглавные
-<<<<<<< HEAD
-=======
-     
->>>>>>> develop
     Parameters
     ----------
     user_str : str
         строка
-<<<<<<< HEAD
-=======
- 
->>>>>>> develop
     Returns
     -------
     str
         строка с заглавными символами
     """
-<<<<<<< HEAD
     user_str = user_str.upper()
     return user_str
 
@@ -36,27 +27,13 @@ def title_str(user_str):
     """
     user_str = user_str.title()
     return user_str
-=======
-    
-    user_str = user_str.upper()
-    return user_str
-
->>>>>>> develop
 
 def main():
 # Ход основной программы
     print("Введите строку")
     user_input = input()
-<<<<<<< HEAD
     print(f"Строка1: {upper_str(user_input)}")
     print(f"Строка2: {title_str(user_input)}")
 
 if __name__ == '__main__':
     main()
-=======
-    user_input = upper_str(user_input)
-    print(f"Строка: {user_input}")
-
-if __name__ == '__main__':
-    main()
->>>>>>> develop
